9dbad2dd37eecc17985de09f80d654ff56af2514
simons1008 <einmorgen@tdmail.de>
2024-08-04 16:56:28 +0200

    TB Server Modul und Testprogramm

--- server_main.py
+++ server_main.py
@@ -4,42 +4,32 @@
 # - Loop:
 #   . holt Daten vom Raspberry Pi
 #   . sendet Daten zum Server
-#   . schläft 5 
+#   . schläft 5 Sekunden
 #   . toggelt die alive-Variable
 # Quelle: https://thingsboard.io/docs/devices-library/raspberry-pi-4/
 # Quelle verteilt auf server_module.py und server_main.py
 
 # Bibliotheken importieren
-import logging.handlers
 import time
 import server_module
 
-ACCESS_TOKEN = "vu3FCDn3RH6VeWgHyX4n"
-THINGSBOARD_SERVER = 'thingsboard.cloud'
-
-logging.basicConfig(level=logging.DEBUG)
-    
-# globale Variable
-client = None
 
 def main():
-    global client, alive
-    client = server_module.TBDeviceMqttClient(THINGSBOARD_SERVER, username=ACCESS_TOKEN)
-    client.connect()
-       
+    server_module.TB_server_connect()
+      
     try: 
-        while not client.stopped:
+        while not server_module.client.stopped:
             attributes, telemetry = server_module.get_data()
-            client.send_attributes(attributes)
-            client.send_telemetry(telemetry)
+            server_module.client.send_attributes(attributes)
+            server_module.client.send_telemetry(telemetry)
             time.sleep(5)
             server_module.alive = not server_module.alive
     except KeyboardInterrupt:
         print("Program terminated by user")
-        client.disconnect()
+        server_module.client.disconnect()
    
 if __name__=='__main__':
-    if ACCESS_TOKEN != "TEST_TOKEN":
+    if server_module.ACCESS_TOKEN != "TEST_TOKEN":
         main()
     else:
         print("Please change the ACCESS_TOKEN variable to match your device access token and run script again.")
--- server_module.py
+++ server_module.py
@@ -1,42 +1,37 @@
 # Inhalt des Moduls
-# - Funktion zum Einlesen von:
-#   . Raspberry Pi Daten
-#   . Netzwerk-Attribute 
+# - Funktion zum Verbinden mit dem Thingsboard Cloud Server 
+# - Funktion zum Einlesen von Netzwerk-Attributen 
 # Quelle: https://thingsboard.io/docs/devices-library/raspberry-pi-4/
-# Quelle verteilt auf server_module.py und server_main.py
 
 import os
+import logging.handlers
 from tb_gateway_mqtt import TBDeviceMqttClient
 
-# alive Variable definieren
+ACCESS_TOKEN = "vu3FCDn3RH6VeWgHyX4n"
+THINGSBOARD_SERVER = 'thingsboard.cloud'
+
+logging.basicConfig(level=logging.DEBUG)
+    
+# globale Variablen
+client = None
 alive = False
+
+# Funktion zum Verbinden mit dem TB Server
+def TB_server_connect():
+    global client
+    client = TBDeviceMqttClient(THINGSBOARD_SERVER, username=ACCESS_TOKEN)
+    client.connect()    
 
 # Funktion zum Einlesen der Daten
 def get_data():
-    cpu_usage = round(float(os.popen('''grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage }' ''').readline().replace('\n', '').replace(',', '.')), 2)
     ip_address = os.popen('''hostname -I''').readline().replace('\n', '').replace(',', '.')[:-1]
     mac_address = os.popen('''cat /sys/class/net/*/address''').readline().replace('\n', '').replace(',', '.')
-    processes_count = os.popen('''ps -Al | grep -c bash''').readline().replace('\n', '').replace(',', '.')[:-1]
-    swap_memory_usage = os.popen("free -m | grep Swap | awk '{print ($3/$2)*100}'").readline().replace('\n', '').replace(',', '.')[:-1]
-#     ram_usage = float(os.popen("free -m | grep Mem | awk '{print ($3/$2) * 100}'").readline().replace('\n', '').replace(',', '.')[:-1])
-    ram_usage = 0.0
-    st = os.statvfs('/')
-    used = (st.f_blocks - st.f_bfree) * st.f_frsize
-    boot_time = os.popen('uptime -p').read()[:-1]
-    avg_load = (cpu_usage + ram_usage) / 2
    
     attributes = {
         'ip_address': ip_address,
         'macaddress': mac_address
     }
     telemetry = {
-        'cpu_usage': cpu_usage,
-        'processes_count': processes_count,
-        'disk_usage': used,
-        'RAM_usage': ram_usage,
-        'swap_memory_usage': swap_memory_usage,
-        'boot_time': boot_time,
-        'avg_load': avg_load,
         'alive': alive
     }
     print(attributes, telemetry)
